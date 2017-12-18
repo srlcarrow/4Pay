52110c5a5a60a54300c7072cccd7f7a3e67689ce
Lanka <lanka@minthrm.com>
2017-12-18 11:10:33 +0530

    config gulp

---
+++ protected/controllers/ComponentsController.php
@@ -0,0 +1,12 @@
+<?php
+
+class ComponentsController extends Controller
+{
+
+
+    public function actionIndex()
+    {
+        $this->render('index');
+    }
+
+}
---
+++ protected/views/components/index.php
@@ -0,0 +1,198 @@
+<div class="row mb-30">
+    <div class="col-md-4 col-md-offset-4">
+
+        <div class="card">
+
+            <div class="card-header">
+                <h1>Login</h1>
+            </div>
+
+            <div class="card-content">
+                <div class="row">
+                    <div class="col-md-12">
+
+                        <div class="row form-wrapper">
+                            <div class="col-md-12 ">
+                                <div class="form-group">
+                                    <label>Username</label>
+                                    <input type="text" class="form-control">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row form-wrapper">
+                            <div class="col-md-12 ">
+                                <div class="form-group">
+                                    <label for="">Password</label>
+                                    <input type="password" class="form-control">
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="card-footer">
+                <div class="row">
+
+                    <!--                    <div class="col-md-12">-->
+                    <!--                        <div class="alert alert-danger">Invalid password</div>-->
+                    <!--                    </div>-->
+
+                    <div class="col-md-12 text-right">
+                        <button type="button" class="btn btn-primary">Login</button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+
+
+<!--<h1>Form</h1>-->
+
+<div class="row mb-30">
+    <div class="col-md-12">
+
+        <div class="card">
+
+            <div class="card-header">
+                <h1>Form </h1>
+            </div>
+
+            <div class="card-content">
+                <div class="row">
+                    <div class="col-md-12">
+
+                        <div class="row form-wrapper">
+                            <div class="col-md-4 ">
+                                <div class="form-group">
+                                    <label>Name</label>
+                                    <input type="text" class="form-control">
+                                </div>
+                            </div>
+
+                            <div class="col-md-4 ">
+                                <div class="form-group">
+                                    <label>Email</label>
+                                    <input type="text" class="form-control">
+                                </div>
+                            </div>
+
+                            <div class="col-md-4 ">
+                                <div class="form-group">
+                                    <label>Phone Number</label>
+                                    <input type="text" class="form-control">
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="row form-wrapper">
+                            <div class="col-md-4 ">
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox"> Check me out
+                                    </label>
+                                </div>
+                            </div>
+
+                            <div class="col-md-4 ">
+                                <label class="radio-inline">
+                                    <input type="radio" name="optionsRadios" value="option1" checked>
+                                    Option one
+                                </label>
+                                <label class="radio-inline">
+                                    <input type="radio" name="optionsRadios" value="option2">
+                                    Option two
+                                </label>
+                            </div>
+
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label for="">Select Option</label>
+                                    <select id="" class="form-control">
+                                        <option>Option 1</option>
+                                        <option>Option 2</option>
+                                        <option>Option 3</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div class="row form-wrapper">
+                            <div class="col-md-12 form-group">
+                                <label for="">Comment</label>
+                                <textarea class="form-control" rows="3"></textarea>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="card-footer">
+                <div class="row">
+
+                    <div class="col-md-12">
+                        <div class="alert alert-success">Save Success</div>
+                    </div>
+
+                    <div class="col-md-12 text-right">
+                        <button type="button" class="btn btn-default">Clear</button>
+                        <button type="button" class="btn btn-primary">Save</button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+
+<div class="row mb-30">
+    <div class="col-md-12">
+        <div class="card">
+
+            <div class="card-header">
+                <h1>Data Table </h1>
+            </div>
+
+            <div class="card-content">
+                <div class="row">
+                    <div class="col-md-12">
+                        <table class="table table-bordered">
+                            <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Phone</th>
+                                <th class="tb-action">Action</th>
+                            </tr>
+                            </thead>
+
+                            <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Natasha Guanwardana</td>
+                                <td>natasha@gmail.com</td>
+                                <td>078 123 2312</td>
+                                <td class="tb-action text-right">
+                                    <button type="button" class="btn btn-sm btn-warning">Edit</button>
+                                    <button type="button" class="btn btn-sm btn-danger">Delete</button>
+                                </td>
+                            </tr>
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
