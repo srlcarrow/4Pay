48c87386a1d86cc5180054ca66b73eef29c4583e
srlcarrow <srlcarrow@gmail.com>
2017-12-19 07:20:10 +0530

    Done

--- protected/components/Controller.php
+++ protected/components/Controller.php
@@ -32,4 +32,57 @@ class Controller extends CController {
         }
     }
 
+    public static function createSearchForEmployee($query, $joinUsing, $page, $limit = NULL, $orderBy = NULL) {
+        $sqlLimit = '';
+        if ($limit == NULL) {
+            $limit = 10;
+            $offset = ($page - 1) * $limit;
+            $sqlLimit = ' LIMIT ' . $limit . ' OFFSET ' . $offset;
+        } elseif ($limit > 0 && $limit != NULL) {
+            $limit = $limit;
+            $offset = ($page - 1) * $limit;
+            $sqlLimit = ' LIMIT ' . $limit . ' OFFSET ' . $offset;
+        }
+
+        $askedQuery = explode("WHERE", $query, 2);
+        $askedJoin = explode("LEFT JOIN", $query, 2);
+        $requestedJoin = '';
+
+        $join = Controller::searchEmployeeJoinCriterias();
+        $where = Controller::searchEmployeeWhereCriterias();
+
+        $askedWhere = '';
+        if (count($askedQuery) > 1) {
+            $askedWhere = $askedQuery[1] == NULL ? '' : ' AND ' . $askedQuery[1];
+        }
+
+        $orderBy = $orderBy != NULL ? 'ORDER BY ' . $orderBy . " DESC" : "";
+        $returnQuery = $askedQuery[0] . $join . ' WHERE ' . $where . $askedWhere . ' ' . $orderBy . $sqlLimit;
+        $returnQueryCount = $askedQuery[0] . $join . ' WHERE ' . $where . $askedWhere . ' ';
+        $result = yii::app()->db->createCommand($returnQuery)->setFetchMode(PDO::FETCH_OBJ)->queryAll();
+        $count = count(yii::app()->db->createCommand($returnQueryCount)->setFetchMode(PDO::FETCH_OBJ)->queryAll());
+
+        return array('result' => $result, 'count' => $count);
+    }
+
+    public static function searchEmployeeJoinCriterias() {
+        $joinCriteria = Yii::app()->db->createCommand()
+                ->leftJoin('emp_employment empl', 'emp.emp_id=empl.ref_emp_id')
+                ->leftJoin('emp_contacts empcon', 'emp.emp_id=empcon.ref_emp_id')
+                ->getText();
+
+        $joinCriteria = explode("SELECT *", $joinCriteria, 2);
+        return $joinCriteria[1];
+    }
+
+    public static function searchEmployeeWhereCriterias() {
+        $str = "emp.emp_id !=0 ";
+
+        if (!empty($_REQUEST['searchEmployeeText']) && $_REQUEST['searchEmployeeText'] != 'undefined') {
+            $str .= " AND  ( emp.emp_full_name Like '%" . $_REQUEST['searchEmployeeText'] . "%' OR emp.emp_name_with_initials Like '%" . $_REQUEST['searchEmployeeText'] . "%' OR emp.emp_display_name Like '%" . $_REQUEST['searchEmployeeText'] . "%' OR emp.epf_no Like '%" . $_REQUEST['searchEmployeeText'] . "%' OR emp.empno Like '%" . $_REQUEST['searchEmployeeText'] . "%' OR emp.emp_nic Like '%" . $_REQUEST['searchEmployeeText'] . "%')";
+        }
+
+        return $str;
+    }
+
 }
---
+++ protected/views/search/searchF1.php
@@ -0,0 +1,78 @@
+<?php
+$form = $this->beginWidget('CActiveForm', array('id' => 'searchF1'));
+?>
+<div class="row mb-30">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-content">
+                <div class="search-box">
+                    <div class="item width-90">
+                        <input type="text" name="searchEmployeeText" class="form-control" placeholder="Search" onkeyup="viewEmployeeData(1)">
+                    </div>
+                    <div class="item width-10">
+                        <button type="button" class="btn btn-advance">Advance</button>
+                    </div>
+                </div>
+
+                <div class="search-advance">
+                    <form action="">
+                        <div class="content">
+                            <div class="row form-wrapper">
+                                <div class="col-md-4 ">
+                                    <div class="form-group">
+                                        <label>Name</label>
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+
+                                <div class="col-md-4 ">
+                                    <div class="form-group">
+                                        <label>Email</label>
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+
+                                <div class="col-md-4 ">
+                                    <div class="form-group">
+                                        <label>Phone Number</label>
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+
+                        <div class="footer">
+                            <div class="row">
+                                <div class="col-md-12 text-right">
+                                    <button type="button" class="btn btn-default btn-close">Close</button>
+                                    <button type="button" class="btn btn-primary">Search</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php $this->endWidget(); ?>
+<div class="col s12 ajaxLoad"></div>
+
+
+<script>
+    $(document).ready(function (e) {
+        viewEmployeeData(1);
+    });
+
+    function viewEmployeeData(page) {
+        $.ajax({
+            type: 'POST',
+            url: "<?php echo Yii::app()->baseUrl . '/' . $controller . '/' . $action; ?>",
+            data: $('#searchF1').serialize() + "&page=" + page,
+            success: function (responce) {
+                $(".ajaxLoad").html(responce);
+            }
+        });
+    }
+</script>
