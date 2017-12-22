9fbf654bb4add9afc94b0ee432d6f6a40320b101
Lanka <lanka@minthrm.com>
2017-12-22 10:21:31 +0530

    Add date and time picker

--- js/common/component.js
+++ js/common/component.js
@@ -1,4 +1,6 @@
+var EVENT = {};
 (function () {
+
 
     // Advance Search.
     $(document).on('click.btn-advance', '.btn-advance', function (e) {
@@ -14,9 +16,32 @@
             $parentBox = $this.parents('.search-advance');
 
         $parentBox.slideUp('fast');
+    });
+
+    $(function () {
+        //Date Picker
+        $(document).find('.input-datepicker').datepicker({
+            language: 'en',
+            dateFormat: 'yyyy-m-dd',
+            autoClose: true,
+            onSelect: function (fdate, date) {
+                $(document).trigger('onDateSelect', [date, fdate]);
+            }
+        });
+
+        // Time Picker
+        $(document).on('focus.input-timepicker', '.input-timepicker', function () {
+            var $this = $(this);
+            $($this).clockpicker({
+                placement: 'top',
+                align: 'left',
+                autoclose: true,
+                afterDone: function () {
+                    $(document).trigger('onTimeSelect', [$this.val(), $this]);
+                }
+            });
+        });
+
     })
-
-
-
 
 })();
