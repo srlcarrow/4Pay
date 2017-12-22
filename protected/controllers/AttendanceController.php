f104606164c32d27dc88364d94535f2b8354c872
srlcarrow <srlcarrow@gmail.com>
2017-12-22 10:48:24 +0530

    RRR

--- protected/controllers/AttendanceController.php
+++ protected/controllers/AttendanceController.php
@@ -9,8 +9,35 @@ class AttendanceController extends Controller {
     public function actionAttendanceTest() {
         $allAttendance = Allattendance::model()->findAll();
         foreach ($allAttendance as $value) {
-            Attendance::dailyAttendance($value->emp_no,$value->checktime,$value->branch_id,$value->punch_by);
+            Attendance::dailyAttendance($value->emp_no, $value->checktime, $value->branch_id, $value->punch_by);
         }
+    }
+
+    
+    public function actionViewAttendanceReport() {
+        $controller = "Attendance";
+        $action = "ViewAttendanceDataReport";
+
+        $reqBasicFields = $this->empBasicFields();
+        $reqAttendanceFields = $this->attendanceFields();
+        $this->render('/search/searchF2', array('controller' => $controller, 'action' => $action, 'reqBasicFields' => $reqBasicFields, 'reqAttendanceFields' => $reqAttendanceFields));
+    }
+
+    public function actionViewAttendanceDataReport() {
+//        var_dump($_POST);exit;
+        $sql = Yii::app()->db->createCommand()
+                ->select('*')
+                ->from('emp_basic emp')
+                ->getText();
+
+        $limit = 5;
+        $data = Controller::createSearchForEmployee($sql, 'emp.emp_id', Yii::app()->request->getPost('page'), $limit, 'emp.epf_no');
+
+        $employeeData = $data['result'];
+        $pageCount = $data['count'];
+        $currentPage = Yii::app()->request->getPost('page');
+
+        $this->renderPartial('/reports/attendance/ajaxLoad/viewAttendanceReportData', array('employeeData' => $employeeData));
     }
 
 }
