558c52674b029c0ed36642df308bfac7038b2838
Lanka <lanka@minthrm.com>
2017-12-23 09:33:06 +0530

    checked array

--- protected/views/search/searchF2.php
+++ protected/views/search/searchF2.php
@@ -6,52 +6,57 @@ $form = $this->beginWidget('CActiveForm', array('id' => 'searchF2'));
         <div class="card">
             <div class="card-content">
                 <div class="search-box">
-                    <div class="item width-90">
-                        <input type="text" name="searchEmployeeText" class="form-control" placeholder="Search" onkeyup="viewEmployeeData(1)">
+                    <div class="item width-80">
+                        <input type="text" name="searchEmployeeText" class="form-control" placeholder="Search"
+                               onkeyup="viewEmployeeData(1)">
+                    </div>
+                    <div class="item width-5">
+                        <button type="button" onclick="viewEmployeeData(1)" class="btn btn-search">Search</button>
                     </div>
                     <div class="item width-10">
                         <button type="button" class="btn btn-advance">Advance</button>
-                        <button type="button" onclick="viewEmployeeData(1)" class="btn btn-search">Search</button>
                     </div>
+
                 </div>
 
                 <div class="search-advance">
-<!--                    <form action="">-->
-                        <div class="content">
-                            <div class="row form-wrapper">
-                                <div class="col-md-4 ">
-                                    <div class="form-group">
-                                        <label>Name</label>
-                                        <input name="name" type="text" class="form-control">
-                                    </div>
-                                </div>
-
-                                <div class="col-md-4 ">
-                                    <div class="form-group">
-                                        <label>Email</label>
-                                        <input type="text" class="form-control">
-                                    </div>
-                                </div>
-
-                                <div class="col-md-4 ">
-                                    <div class="form-group">
-                                        <label>Phone Number</label>
-                                        <input type="text" class="form-control">
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
-
-                        <div class="footer">
-                            <div class="row">
-                                <div class="col-md-12 text-right">
-                                    <button type="button" class="btn btn-default btn-close">Close</button>
-                                    <button type="button" onclick="viewEmployeeData(1)" class="btn btn-primary">Search</button>
+                    <!--                    <form action="">-->
+                    <div class="content">
+                        <div class="row form-wrapper">
+                            <div class="col-md-4 ">
+                                <div class="form-group">
+                                    <label>Name</label>
+                                    <input name="name" type="text" class="form-control">
                                 </div>
                             </div>
+
+                            <div class="col-md-4 ">
+                                <div class="form-group">
+                                    <label>Email</label>
+                                    <input type="text" class="form-control">
+                                </div>
+                            </div>
+
+                            <div class="col-md-4 ">
+                                <div class="form-group">
+                                    <label>Phone Number</label>
+                                    <input type="text" class="form-control">
+                                </div>
+                            </div>
+
                         </div>
-<!--                    </form>-->
+                    </div>
+
+                    <div class="footer">
+                        <div class="row">
+                            <div class="col-md-12 text-right">
+                                <button type="button" class="btn btn-default btn-close">Close</button>
+                                <button type="button" onclick="viewEmployeeData(1)" class="btn btn-primary">Search
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <!--                    </form>-->
                 </div>
             </div>
         </div>
@@ -68,7 +73,8 @@ $form = $this->beginWidget('CActiveForm', array('id' => 'searchF2'));
                     <div class="col-md-2 ">
                         <div class="checkbox">
                             <label>
-                                <input name="<?php echo $key; ?>" value="1" type="checkbox"><?php echo $reqBasicField; ?>
+                                <input name="<?php echo $key; ?>" value="1"
+                                       type="checkbox"><?php echo $reqBasicField; ?>
                             </label>
                         </div>
                     </div>
@@ -89,7 +95,8 @@ $form = $this->beginWidget('CActiveForm', array('id' => 'searchF2'));
                     <div class="col-md-2 ">
                         <div class="checkbox">
                             <label>
-                                <input name="<?php echo $key; ?>" value="1" type="checkbox"><?php echo $reqAttendanceField; ?>
+                                <input name="<?php echo $key; ?>" value="1"
+                                       type="checkbox"><?php echo $reqAttendanceField; ?>
                             </label>
                         </div>
                     </div>
@@ -98,9 +105,10 @@ $form = $this->beginWidget('CActiveForm', array('id' => 'searchF2'));
             }
             ?>
 
+            <input type="hidden" class="checkedInput">
 
         </div>
-    </div>    
+    </div>
 </div>
 
 <?php $this->endWidget(); ?>
@@ -108,11 +116,33 @@ $form = $this->beginWidget('CActiveForm', array('id' => 'searchF2'));
 
 
 <script>
+
+    var getSelectedCheckbox = function () {
+        var result = [];
+        $('input[type="checkbox"]:checked').each(function () {
+            var name = $(this).attr('name');
+            result.push({name: name});
+        });
+
+        console.log(result);
+        console.log(JSON.stringify(result));
+        console.log(typeof JSON.stringify(result));
+        return result;
+    };
+
     $(document).ready(function (e) {
         viewEmployeeData(1);
     });
 
+
     function viewEmployeeData(page) {
+
+        var selectedCheckbox = getSelectedCheckbox();
+
+        var json = JSON.stringify(selectedCheckbox);
+
+        $('.checkedInput').val(json);
+
         $.ajax({
             type: 'POST',
             url: "<?php echo Yii::app()->baseUrl . '/' . $controller . '/' . $action; ?>",
