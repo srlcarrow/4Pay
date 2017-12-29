fe8261182f54681bdec52660d818f63539bf494f
Lanka <lanka@minthrm.com>
2017-12-29 09:23:37 +0530

    Chaged Calender

--- protected/config/database.php
+++ protected/config/database.php
@@ -6,7 +6,7 @@ return array(
 	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
 	
-	'connectionString' => 'mysql:host=localhost;dbname=tts',
+	'connectionString' => 'mysql:host=localhost:3307;dbname=p1',
 	'username' => 'root',
 	'password' => '',
 	'charset' => 'utf8',
