8853308eeb018c69d21afa22bd8fb26791c36f39
Tuck <srlctuck@gmail.com>
2017-12-29 08:14:23 +0530

    test456

--- protected/views/employee/ajaxLoad/profile/leave.php
+++ protected/views/employee/ajaxLoad/profile/leave.php
@@ -9,9 +9,10 @@
                         <div class="form-group">
                             <label for="">Leave Type</label>
                             <select id="type" class="form-control">
-                                <option>Option 1</option>
-                                <option>Option 2</option>
-                                <option>Option 3</option>
+                                <option>Annual Leave</option>
+                                <option>Casual Leave</option>
+                                <option>Medical Leave</option>
+                                <option>Medical Leave</option>
                             </select>
                         </div>
                     </div>
