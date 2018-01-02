3c5b239ab83caf719c044feeb44f133f1da33050
Lanka <lanka@minthrm.com>
2018-01-02 23:59:32 +0530

    changed checkbox

--- protected/config/database.php
+++ protected/config/database.php
@@ -6,14 +6,9 @@ return array(
 	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
 
-	
-<<<<<<< HEAD
 	'connectionString' => 'mysql:host=localhost:3307;dbname=p1',
-=======
-	'connectionString' => 'mysql:host=localhost:3307;dbname=p1_new2',
->>>>>>> 6aa65a1484ff804153ca7a5178eca39bb6d02b98
 	'username' => 'root',
-	'password' => 'chathu123',
+	'password' => '',
 	'charset' => 'utf8',
 
 );
