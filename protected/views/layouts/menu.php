dc9ea03e8202d44cae93471541e3996f6d99652e
Lanka <lanka@minthrm.com>
2018-01-05 16:51:16 +0530

    Add logo and notification

--- protected/views/layouts/menu.php
+++ protected/views/layouts/menu.php
@@ -3,14 +3,19 @@ $mainLinks = AdmLinks::model()->findAllByAttributes(array('lnk_parent_id' => 0, 
 ?>
 <nav class="navbar navbar-default">
     <div class="container-fluid">
-
+        <div class="navbar-header">
+            <a class="navbar-brand" href="<?php echo Yii::app()->baseUrl; ?>">
+                <img src="<?php echo Yii::app()->baseUrl; ?>/uploads/company/logo/logo.png" alt="Logo">
+            </a>
+        </div>
         <ul class="nav navbar-nav navbar-right">
             <?php
             foreach ($mainLinks as $mainLink) {
                 $subLinks = AdmLinks::model()->findAllByAttributes(array('lnk_parent_id' => $mainLink->lnk_id), array('order' => 'lnk_order ASC'));
                 ?>
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?php echo strtoupper($mainLink->lnk_name); ?> <span class="caret"></span></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                       aria-expanded="false"><?php echo strtoupper($mainLink->lnk_name); ?> <span class="caret"></span></a>
                     <ul class="dropdown-menu">
                         <?php
                         foreach ($subLinks as $subLink) {
@@ -21,10 +26,21 @@ $mainLinks = AdmLinks::model()->findAllByAttributes(array('lnk_parent_id' => 0, 
                         }
                         ?>
                     </ul>
-                </li>                
+                </li>
                 <?php
             }
             ?>
+            <li class="dropdown notification">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                   aria-expanded="false">
+                    <i class="bell"></i>
+                    <span class="n-count">12</span>
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Leave</a></li>
+                    <li><a href="#">Lorem ipsum.</a></li>
+                </ul>
+            </li>
             <li><a href="<?php echo Yii::app()->request->baseUrl . '/Site/Logout' ?>">Logout</a></li>
         </ul>
 
