b776c3d0058be130c33e116f69eaa863108bf05f
sanjeewa <sanjeewa@infoseek.lk>
2018-01-07 21:00:43 +0530

    fixed db issue

--- protected/config/database.php
+++ protected/config/database.php
@@ -5,7 +5,7 @@ return array(
 	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
 
-	'connectionString' => 'mysql:host=localhost;dbname=tty',
+	'connectionString' => 'mysql:host=localhost;dbname=tty_l',
 
 
 	'username' => 'root',
