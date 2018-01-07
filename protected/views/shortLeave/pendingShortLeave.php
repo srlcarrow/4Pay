52ff44950951aebce9c14ac5456ddd6ae45a0968
Chathuranga <srlctuck@gmail.com>
2018-01-07 08:35:24 +0530

    test

--- protected/views/shortLeave/pendingShortLeave.php
+++ protected/views/shortLeave/pendingShortLeave.php
@@ -33,7 +33,7 @@
                                         <td><?php echo $pendingSLeaves->short_leave_date; ?></td>
                                         <td><?php echo date("H:i", strtotime($pendingSLeaves->start_time)); ?></td>
                                         <td><?php echo date("H:i", strtotime($pendingSLeaves->end_time)); ?></td>
-                                        <td><input type="text" name="rejectReason_<?php echo $pendingSLeaves->short_lv_id;   ?>" id="rejectReason_<?php echo $pendingSLeaves->short_lv_id;   ?>" class="form-control" value="" placeholder="Reject Reason" /></td>
+                                        <td><input type="text" name="rejectReason_<?php echo $pendingSLeaves->short_lv_id; ?>" id="rejectReason_<?php echo $pendingSLeaves->short_lv_id; ?>" class="form-control" value="" placeholder="Reject Reason" /></td>
                                         <td class="tb-action text-right">
                                             <button type="button" onclick="approveShortLeave('<?php echo $pendingSLeaves->short_lv_id; ?>')" class="btn btn-sm btn-warning">App</button> 
                                             <button type="button" onclick="rejectShortLeave('<?php echo $pendingSLeaves->short_lv_id; ?>')"  class="btn btn-sm btn-danger">Reject</button>
@@ -55,26 +55,74 @@
 <script>
 
     function approveShortLeave(id) {
-        $.ajax({
-            type: 'POST',
-            url: "<?php echo Yii::app()->baseUrl . '/ShortLeave/ApproveShortLeave'; ?>",
-            data: {shortLeaveId: id},
-            success: function (responce) {
-                location.reload();
-            }
-        });
+        swal({
+            title: "Are you sure?",
+            text: "You will not have this data anymore!",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Yes, delete it!",
+            cancelButtonText: "No, cancel plx!",
+            closeOnConfirm: true,
+            closeOnCancel: true
+        },
+                function (isConfirm) {
+                    if (isConfirm) {
+                        $.ajax({
+                            type: 'POST',
+                            url: "<?php echo Yii::app()->baseUrl . '/ShortLeave/ApproveShortLeave'; ?>",
+                            data: {shortLeaveId: id},
+                            success: function (res) {
+                                if (res) {
+                                    setTimeout(function () {
+                                        location.reload();
+                                    }, 500);
+                                } else {
+                                    swal("Cancelled", "You are not allowed to delete today's short leaves.", "error");
+                                }
+                            }
+                        });
+                    }
+                }
+        );
     }
 
     function rejectShortLeave(id) {
         var reason = document.getElementById("rejectReason_" + id).value;
-        $.ajax({
-            type: 'POST',
-            url: "<?php echo Yii::app()->baseUrl . '/ShortLeave/RejectShortLeave'; ?>",
-            data: {shortLeaveId: id, reason: reason},
-            success: function (responce) {
-                location.reload();
-            }
-        });
+        if (reason == '') {
+            $("#rejectReason_" + id).focus();
+        } else {
+            swal({
+                title: "Are you sure?",
+                text: "You will not have this data anymore!",
+                type: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "Yes, delete it!",
+                cancelButtonText: "No, cancel plx!",
+                closeOnConfirm: true,
+                closeOnCancel: true
+            },
+                    function (isConfirm) {
+                        if (isConfirm) {
+                            $.ajax({
+                                type: 'POST',
+                                url: "<?php echo Yii::app()->baseUrl . '/ShortLeave/RejectShortLeave'; ?>",
+                                data: {shortLeaveId: id, reason: reason},
+                                success: function (res) {
+                                    if (res) {
+                                        setTimeout(function () {
+                                            location.reload();
+                                        }, 500);
+                                    } else {
+                                        swal("Cancelled", "You are not allowed to delete today's short leaves.", "error");
+                                    }
+                                }
+                            });
+                        }
+                    }
+            );
+        }
     }
 
 </script>
